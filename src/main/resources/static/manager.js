a5fd802b5edb2126275f0feef16909f577aff704
Senkidev <dante.vilchesc@gmail.com>
2022-11-24 16:50:12 -0300

    add delete and edit to each client

--- src/main/resources/static/manager.js
+++ src/main/resources/static/manager.js
@@ -35,28 +35,116 @@ createApp({
 			if (
 				this.inputName != "" &&
 				this.inputLastName != "" &&
-				this.inputEmail != "" &&
-				this.inputEmail.includes("@" && ".")
+				this.inputEmail != ""
 			) {
-				this.postClient(this.client);
-				this.inputEmail = "";
-				this.inputLastName = "";
-				this.inputName = "";
-				Swal.fire({
-					icon: "success",
-					title: "Ha agregado un nuevo cliente",
-				});
+				if (this.inputEmail.includes("@") && this.inputEmail.includes(".")) {
+					this.postClient(this.client);
+					this.inputEmail = "";
+					this.inputLastName = "";
+					this.inputName = "";
+					Swal.fire({
+						icon: "success",
+						title: "A new client has been added",
+					});
+				} else {
+					Swal.fire({
+						icon: "error",
+						title: "Email is not valid",
+						text: "Please correct the email format",
+					});
+				}
 			} else {
 				Swal.fire({
 					icon: "error",
-					title: "Los campos están vacíos",
-					text: "Por favor rellene los campos",
+					title: "Fields are empty",
+					text: "Please complete the fields",
 				});
 			}
-			this.loadData();
 		},
 		postClient(client) {
-			axios.post(this.url, client);
+			axios.post(this.url, client).then((response) => this.loadData());
+		},
+		deleteClient(client) {
+			let idIndex = client._links.client.href.lastIndexOf("/");
+			let id = client._links.client.href.substring(idIndex + 1);
+
+			Swal.fire({
+				title: "Are you sure that you want to delete this client?",
+				text: "You won't be able to revert this",
+				icon: "error",
+				showCancelButton: true,
+				confirmButtonColor: "#3085d6",
+
+				cancelButtonColor: "#d33",
+				confirmButtonText: "Yes, delete this client",
+			}).then((result) => {
+				if (result.isConfirmed) {
+					Swal.fire("Deleted!", "The client has been deleted.", "success");
+					axios
+						.delete(`http://localhost:8080/clients/${id}`)
+						.then((response) => this.loadData());
+				}
+			});
+		},
+		async modifyClient(client) {
+			let idIndex = client._links.client.href.lastIndexOf("/");
+			let id = client._links.client.href.substring(idIndex + 1);
+			const { value: formValues } = await Swal.fire({
+				title: "Edit Client",
+				html:
+					`<label>First Name<input id="swal-input1" class="swal2-input" value=${client.clientName}></label>` +
+					`<label>Last Name<input id="swal-input2" class="swal2-input" value=${client.clientLastName}></label>` +
+					`<label>Client Email<input type="email "id="swal-input3" class="swal2-input" value=${client.clientEmail}></></label>`,
+				focusConfirm: false,
+				confirmButtonText: "Save",
+				confirmButtonColor: "#3085d6",
+				showCancelButton: true,
+				preConfirm: () => {
+					return [
+						document.getElementById("swal-input1").value,
+						document.getElementById("swal-input2").value,
+						document.getElementById("swal-input3").value,
+					];
+				},
+			});
+			let inputSwalFirstName = document.getElementById("swal-input1").value;
+			let inputSwalLastName = document.getElementById("swal-input2").value;
+			let inputSwalEmail = document.getElementById("swal-input3").value;
+			if (formValues) {
+				this.client = {
+					clientName: inputSwalFirstName,
+					clientLastName: inputSwalLastName,
+					clientEmail: inputSwalEmail,
+				};
+				if (
+					inputSwalFirstName != "" &&
+					inputSwalLastName != "" &&
+					inputSwalEmail != ""
+				) {
+					if (inputSwalEmail.includes("@") && inputSwalEmail.includes(".")) {
+						axios
+							.put(`http://localhost:8080/clients/${id}`, this.client)
+							.then((response) => this.loadData());
+
+						Swal.fire({
+							icon: "success",
+							title: "The client has been edited",
+						});
+					} else {
+						Swal.fire({
+							icon: "error",
+							title: "Email is not valid",
+							text: "Please correct the email format",
+						});
+					}
+				} else {
+					Swal.fire({
+						icon: "error",
+						title: "Fields are empty",
+						text: "Please complete the fields",
+					});
+				}
+			}
 		},
 	},
 	computed: {},
