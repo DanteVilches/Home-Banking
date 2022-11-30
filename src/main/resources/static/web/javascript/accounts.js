8d0b0d1289a9a58e30e1d3a032f3a44e35dcab21
Senkidev <dante.vilchesc@gmail.com>
2022-11-30 13:21:49 -0300

    add dinamyc to colors from accounts

--- src/main/resources/static/web/javascript/accounts.js
+++ src/main/resources/static/web/javascript/accounts.js
@@ -6,10 +6,53 @@ Vue.createApp({
 			client: {},
 			clientName: "",
 			accounts: [],
+			arrayOfColours: ["#fee4cb", "#e9e7fd", "#ffd3e2", "#c8f7dc", "#d5deff"],
 		};
 	},
 	created() {
 		this.loadData();
+		this.shuffle();
+		console.log(this.arrayOfColours[0]);
+		document.addEventListener("DOMContentLoaded", function () {
+			let modeSwitch = document.querySelector(".mode-switch");
+
+			modeSwitch.addEventListener("click", function () {
+				document.documentElement.classList.toggle("dark");
+				modeSwitch.classList.toggle("active");
+
+				if (document.documentElement.classList.contains("dark")) {
+					localStorage.setItem("dark-mode", "true");
+				} else {
+					localStorage.setItem("dark-mode", "false");
+				}
+			});
+
+			if (localStorage.getItem("dark-mode") === "true") {
+				document.documentElement.classList.add("dark");
+				modeSwitch.classList.add("active");
+			} else {
+				document.documentElement.classList.remove("dark");
+				modeSwitch.classList.remove("active");
+			}
+
+			let listView = document.querySelector(".list-view");
+			let gridView = document.querySelector(".grid-view");
+			let projectsList = document.querySelector(".project-boxes");
+
+			listView.addEventListener("click", function () {
+				gridView.classList.remove("active");
+				listView.classList.add("active");
+				projectsList.classList.remove("jsGridView");
+				projectsList.classList.add("jsListView");
+			});
+
+			gridView.addEventListener("click", function () {
+				gridView.classList.add("active");
+				listView.classList.remove("active");
+				projectsList.classList.remove("jsListView");
+				projectsList.classList.add("jsGridView");
+			});
+		});
 	},
 
 	methods: {
@@ -18,51 +61,15 @@ Vue.createApp({
 				.get(this.url)
 				.then((data) => {
 					this.client = data.data;
-
 					this.accounts = this.client.accountDTO;
 					this.clientName = `${this.client.clientName} ${this.client.clientLastName}`;
-					console.log(this.accounts);
 				})
 				.catch((error) => console.log(error));
+		},
+		shuffle() {
+			return this.arrayOfColours.sort(() => Math.random() - 0.5);
 		},
 	},
 
 	computed: {},
 }).mount("#app");
-
-document.addEventListener("DOMContentLoaded", function () {
-	let modeSwitch = document.querySelector(".mode-switch");
-
-	modeSwitch.addEventListener("click", function () {
-		document.documentElement.classList.toggle("dark");
-		modeSwitch.classList.toggle("active");
-	});
-
-	let listView = document.querySelector(".list-view");
-	let gridView = document.querySelector(".grid-view");
-	let projectsList = document.querySelector(".project-boxes");
-
-	listView.addEventListener("click", function () {
-		gridView.classList.remove("active");
-		listView.classList.add("active");
-		projectsList.classList.remove("jsGridView");
-		projectsList.classList.add("jsListView");
-	});
-
-	gridView.addEventListener("click", function () {
-		gridView.classList.add("active");
-		listView.classList.remove("active");
-		projectsList.classList.remove("jsListView");
-		projectsList.classList.add("jsGridView");
-	});
-
-	document.querySelector(".messages-btn").addEventListener("click", function () {
-		document.querySelector(".messages-section").classList.add("show");
-	});
-
-	document
-		.querySelector(".messages-close")
-		.addEventListener("click", function () {
-			document.querySelector(".messages-section").classList.remove("show");
-		});
-});
